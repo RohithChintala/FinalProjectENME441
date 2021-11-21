07b2e7bba2ba5cfbf218e3306ac1318d5e4a3c30
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-11-21 16:26:32 +0000

    i

--- testsecret.py
+++ testsecret.py
@@ -43,8 +43,8 @@ def get_busy_times_from_google_calendar():
 
     # Get the start and end times from all of the events.
     busy_times = []
-    meetings = []
-    global i = 0
+    meetings = [] * 10
+    i = 0
     for event in parsed_response["items"]:
         event_start = datetime.fromisoformat(event["start"]["dateTime"])
         event_end = datetime.fromisoformat(event["end"]["dateTime"])
