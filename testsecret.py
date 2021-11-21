fd029bceb7acc48b353716845b78e5a8d218a2f1
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-11-21 16:12:44 +0000

    s

--- testsecret.py
+++ testsecret.py
@@ -69,7 +69,7 @@ def main():
 
   busy_times = get_busy_times_from_google_calendar()
   busy_right_now = check_if_busy(busy_times, now)
-  print()
+  print(busy_right_now)
 
 if __name__ == "__main__":
     main()
