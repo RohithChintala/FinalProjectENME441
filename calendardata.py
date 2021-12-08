02bd9da1627150bb7e1042e2f7e5146ea0284aae
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-12-08 20:38:42 +0000

    a

--- calendardata.py
+++ calendardata.py
@@ -65,7 +65,7 @@ def get_busy_times_from_google_calendar():
         day[i] = event_start.strftime("%d")
         hour[i] = event_start.strftime("%H")  
         minute[i] = event_start.strftime("%M")   
-        #print(day[i],hour[i],minute[i],currentday)
+        print(day[i],hour[i],minute[i],currentday)
         #need to look into different months
         if int(day[i]) == int(currentday)+1:
           if int(hour[i]) < wakeuptime:
