a850b02ca2b70805d6fd3a2bac4e3e2f0f34ed01
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-12-10 18:42:34 +0000

    d

--- finalproject_background.py
+++ finalproject_background.py
@@ -37,7 +37,6 @@ while True: #runs continuously
     m = [data['mhours'],data['mmin'],data['mtime']]
     n = [data['nhours'],data['nmin'],data['ntime']]
     r = [data['rhours'],data['rmin'],data['rtime']]
-    t = data['test']
   LOCAL_TIMEZONE = "America/New_York"
   timezone = pytz.timezone(LOCAL_TIMEZONE)
   now = timezone.localize(datetime.now())
@@ -67,7 +66,7 @@ while True: #runs continuously
   #print(wake)
   #print('stop')
   #time.sleep(20)
-  if test == 'test':
+  if data['test'] == 'testing':
     for i in range(1, len(song_1)):
       buzzloop(GPIO.input(buttonPin),song_1[i])
   if int(currenthour) == r[0]:
