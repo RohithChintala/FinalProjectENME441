15551f89fe4afc7ba901d6d442f8d60df8a28850
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-12-11 17:40:49 +0000

    s

--- finalproject_background.py
+++ finalproject_background.py
@@ -83,11 +83,13 @@ while True: #runs continuously
     for i in range(1, len(song)):
       buzzloop(GPIO.input(buttonPin),song[i],beat[i])
     buzzdestroy()
-  if int(currenthour) == r[0]:
-    if int(currentminute) == r[2]:
+  if int(currenthour) == int(r[0]):
+    if int(currentminute) == int(r[1]):
+      print('refresh')
   #if int(currenthour) == int(currenthour): 
     #if int(currentminute) == int(currentminute):
       wake, currentday = getcalendardata()
+      print(wake)
       morningh = int(wake[0])- int(m[0])
       morningm = int(wake[1])- int(m[1])
       if morningm < 0:
